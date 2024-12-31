d39e9e2641ee05a6369a0acf76cdcd0641408ba0
El-Hadj Ilyes Abd-Lillah <ilyes1204@gmail.com>
2024-12-31 01:32:41 +0100

    (redis): rate limiting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta
